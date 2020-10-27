5bf65faca39cd15de35dce5c3f210d221442400c
Eduardo <eduuhliima97@gmail.com>
2020-10-27 13:31:24 -0300

    atualizações

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
-$(document).ready(function(){
-  
-
-$(".div-pai").mouseenter(function(){
-  $(".text-model").show(3000);
-});
-  $(".div-pai").mouseleave(function(){
-    $(".text-model").hide(2000);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+//Validation Form
+function validate() {
+  var x = document.getElementById("input_try").value;
+  if (x == "" || x == null) {
+    alert("Name necessary");
+  }
+}
