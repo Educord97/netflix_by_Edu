40c13d09c75c97952ffbb2bdfbee8e8c524bf100
Eduardo <eduuhliima97@gmail.com>
2020-12-21 13:53:36 -0300

    identação

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function validate() {
-
-  output.innerHTML = '';
+   output.innerHTML = '';
   var email = myForm.email;
-  if (email.value == "") {
-    document.getElementById("output").innerHTML = "The email is required!";
-
-    email.focus();
+     if (email.value == "") {
+       document.getElementById("output").innerHTML = "The email is required!";
+        email.focus();
   }
 }
